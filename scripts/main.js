a420b7b2cfe2cb3215500737eebf11e8777d3bbd
DMVTJ <aldenborjacarp48@gmail.com>
2023-06-05 21:52:48 -0300

    probando deploy

--- scripts/main.js
+++ scripts/main.js
@@ -14,3 +14,20 @@ menubtn.addEventListener('click' , ()=> {
 
 
 
+// const fora = document.getElementById('form')
+// fora.addEventListener('submit', validarFormulario)
+const form = "hola"
+
+function asdsad (e){
+    // e.preventDefaylt();
+
+    // let nombre = document.getElementById("nombre").value
+    // let mensaje = document.getElementById("mensaje").value
+
+    let hola = form
+    console.log(hola);
+}
+
+asdsad()
+
+console.log("adios");
