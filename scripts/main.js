db0715c8f83e0b8c650ea822bfa9f94d11811b97
DMVTJ <aldenborjacarp48@gmail.com>
2023-06-06 01:14:21 -0300

    semiterminado

--- scripts/main.js
+++ scripts/main.js
@@ -16,31 +16,38 @@ menubtn.addEventListener('click' , ()=> {
 
 
 
-function comentario(){
+function comentarioCaja(){
     const nombre = document.getElementById("nombre").value;
     const mensaje = document.getElementById("mensaje").value;
+    const comentario = document.getElementById("coments")
 
     if(nombre == "" && mensaje == ""){
-
+        //si no llena ningun espacio le aparece esta alerta
         alert("Es necesario que ingrese un nombre y un comentario")
         document.getElementById("nombre").focus();
     }
 
     else if(nombre == ""){
-
+        //si no llena ningun espacio le aparece esta alerta
         alert("Es necesario que ingrese un nombre")
         document.getElementById("nombre").focus();
     }
 
     else if(mensaje == ""){
-
+        //si no llena ningun espacio le aparece esta alerta
         alert("Es necesario que ingrese un comentario")
         document.getElementById("mensaje").focus();
     }
     
     else{
-
-        console.log("su nombre es " + nombre + "y su comentario es " + mensaje);
-
+        //Si esta todo bien envia el comentario y se guarda para poder enviar otro comentario
+        comentario.innerHTML += `
+        <div class="border">
+        <h4>${nombre}</h4>
+        <p>${mensaje}</p>
+        </div>`
+        //Se vuelven los datos a la nada
+        document.getElementById("nombre").value = ""
+        document.getElementById("mensaje").value = ""
     }
 }
